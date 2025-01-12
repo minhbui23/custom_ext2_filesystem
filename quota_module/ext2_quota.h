80fbbd69daaa6520ccd6adc122e7242a1739a771
minhbui23 <j.buiminh@gmail.com>
2025-01-12 15:48:46 +0700

    add comment for quota_module

--- quota_module/ext2_quota.h
+++ quota_module/ext2_quota.h
@@ -6,25 +6,25 @@
 #include <linux/kernel.h>
 #include <linux/kprobes.h>
 
-// Định nghĩa cấu trúc ext2_quota_info
+// ext2_quota_info
 struct ext2_quota_info {
-    uid_t uid;                // ID người dùng
-    unsigned long limit;      // Giới hạn dung lượng sử dụng
-    unsigned long usage;      // Dung lượng đã sử dụng
-    struct hlist_node node;   // Node cho bảng băm
+    uid_t uid;                
+    unsigned long limit;      
+    unsigned long usage;      
+    struct hlist_node node;   // Node for hash table
 };
 
 #define QUOTA_HASH_BITS 8
 extern DEFINE_HASHTABLE(quota_hash_table, QUOTA_HASH_BITS);
 
-// Khai báo các hàm
+// Define func prototype
 void add_quota(uid_t uid, unsigned long limit, unsigned long usage);
 void show_quota_table(void);
 void cleanup_quota_table(void);
 bool check_quota(uid_t uid, unsigned long size);
 void update_quota(uid_t uid, unsigned long size);
 
-// Khai báo kprobe và các handler
+// Define kprobe func
 extern struct kprobe kp_write;
 extern int write_pre_handler(struct kprobe *p, struct pt_regs *regs);
 extern void write_post_handler(struct kprobe *p, struct pt_regs *regs, unsigned long flags);
@@ -32,6 +32,7 @@ extern void write_post_handler(struct kprobe *p, struct pt_regs *regs, unsigned 
 extern struct kprobe kp_unlink;
 extern void unlink_post_handler(struct kprobe *p, struct pt_regs *regs, unsigned long flags);
 
+//Define file I/O func
 extern void save_quota_to_file(void);
 extern void load_quota_from_file(void);
 
